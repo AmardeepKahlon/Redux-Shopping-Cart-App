ee3eab541e3de02fa638a8d8a75e7386b59e04a0
AmardeepKahlon <asingh@innow8apps.com>
2023-12-13 10:57:05 +0530

    add to cart redux working

--- src/store/auth-slice.js
+++ src/store/auth-slice.js
@@ -3,7 +3,7 @@ import { createSlice } from "@reduxjs/toolkit";
 const authSlice = createSlice({
     name: 'auth',
     initialState: {isLoggedIn: false},
-    reduscers: {
+    reducers: {
         login(state) {
             state.isLoggedIn = true;
         },
